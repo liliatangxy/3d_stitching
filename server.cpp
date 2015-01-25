2603bfdb9da7f8237f079ec4ec056d0ea22d71ce
Lilia Tang <liliatangxy@gmail.com>
2015-01-24 21:37:43 -0800

    fixed structure

---
+++ server.cpp
@@ -0,0 +1,11 @@
+#include <server.h>
+
+class ImageHandler {
+	public:
+		ImageHandler() {}
+
+		bool initialization() {
+			return geometry_initialize() && image_loader_initialize();
+		}
+		
+}
---
+++ server.h
@@ -0,0 +1,25 @@
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/PosixThreadFactory.h>
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/server/TThreadPoolServer.h>
+#include <thrift/server/TThreadedServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+#include <thrift/TToString.h>
+
+#include <insight3d/insight3d/geometry_structures.h>
+#include <insight3d/insight3d/core_image_loader.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::concurrency;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::server;
+
+bool initialization();
